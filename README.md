5bfa3bf20434f1b55299363ba262bb3dbab7d253
Marcos Marado <mindboosternoori@gmail.com>
2022-02-18 12:20:44 +0000

    first version of the app

---
+++ README.md
@@ -0,0 +1,6 @@
+# CustomBroadcastReceiver
+
+This might be useful for developers, and needs customization:
+
+Edit `AndroidManifest.xml` to change the example intents in there into the
+broadcasts you want this app to listen to.
---
+++ src/main/com/marado/CustomBroadcastReceiver/CustomBroadcastReceiver.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2022 Marcos Marado <mindboosternoori@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.marado.CustomBroadcastReceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * An (explicit) broadcast receiver triggered by an intent of the time defined
+ * in the application's manifest.
+ */
+public class CustomBroadcastReceiver extends BroadcastReceiver {
+    final static String TAG = "CustomBroadcastReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.w(TAG, "Intent action received: " + intent + ", action=" + intent.getAction());
+        return;
+    }
+
+}
